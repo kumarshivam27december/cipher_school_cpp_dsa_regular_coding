364740c8f1223585bf5d3ff28259f4cc661a4852
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-08-03 01:12:04 +0530

    version 3.0

--- code2.cpp/00_diff_between_procedure_vs_oops.cpp
+++ code2.cpp/00_diff_between_procedure_vs_oops.cpp
@@ -15,6 +15,11 @@ now if we wanted to make data of 100 user we will make array of type struct
 if i wanted to add 100 rupees in the bank balances 
 
 
+it is difficult to add another account of a person in this way we can say this that procedural approach
+is very different from real life problems
+
+so the concept of oops come here
+
 
 
 */
