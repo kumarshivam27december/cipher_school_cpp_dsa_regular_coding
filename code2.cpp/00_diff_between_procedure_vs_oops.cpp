01976d27c81c12dc767bd35702054e8826f621e3
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-08-02 22:08:27 +0530

    version 2.0

---
+++ code2.cpp/00_diff_between_procedure_vs_oops.cpp
@@ -0,0 +1,20 @@
+// procedural approach
+
+/*
+struct users{
+    int age;
+    string name;
+    bool adult;
+    float bankbalance;
+    int last_ten_transaction[10] ;
+}
+
+//
+now if we wanted to make data of 100 user we will make array of type struct
+
+if i wanted to add 100 rupees in the bank balances 
+
+
+
+
+*/
