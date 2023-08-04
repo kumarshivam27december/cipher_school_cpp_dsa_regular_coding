afcf55a1cfca6e0420d56ee4a9c21969172e7dcf
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-08-04 23:31:46 +0530

    version 5.0

---
+++ day0/00_basics.cpp
@@ -0,0 +1,112 @@
+/*
+
+C++ is a powerful and versatile programming language widely used for system development, 
+game development, application software, and more. Here are some fundamental concepts and 
+syntax you might find useful:
+
+
+
+Basic Structure:
+
+
+
+C++ programs have a main() function which is the entry point.
+Statements are terminated with a semicolon ;.
+
+
+Data Types:
+
+C++ has built-in data types like int, double, char, bool, etc.
+You can use the sizeof() operator to determine the size of a data type in bytes.
+
+
+
+Variables and Constants:
+
+Variables are declared with a data type and an optional initial value.
+Constants are declared with the const keyword.
+Use cin for input and cout for output.
+
+
+
+Operators:
+
+Arithmetic operators: +, -, *, /, %.
+Relational operators: ==, !=, <, >, <=, >=.
+Logical operators: &&, ||, !.
+
+
+
+
+Control Structures:
+
+if, else if, else for conditional branching.
+switch for multi-way branching.
+for, while, do-while for loops.
+
+
+
+
+Functions:
+
+Functions are defined with a return type, name, parameters, and a body.
+You can pass arguments by value or reference.
+Recursive functions call themselves.
+
+
+
+
+Arrays and Pointers:
+
+Arrays hold a fixed-size collection of elements of the same data type.
+Pointers store memory addresses.
+Arrays and pointers are closely related.
+
+
+
+Classes and Objects:
+
+C++ supports object-oriented programming.
+Classes define the blueprint for objects.
+Objects are instances of classes.
+
+
+
+
+Inheritance and Polymorphism:
+
+Inheritance allows creating a new class based on an existing class.
+Polymorphism allows objects of different classes to be treated as objects of a common base class.
+
+
+
+Dynamic Memory Allocation:
+
+new allocates memory on the heap.
+delete deallocates memory.
+
+
+
+
+Exception Handling:
+
+try, catch, and throw are used for exception handling.
+
+
+
+Standard Template Library (STL):
+
+Provides useful classes and functions for common tasks.
+Includes containers like vector, list, map, etc.
+Includes algorithms like sorting and searching.
+Remember, these are just some key concepts. 
+
+
+
+
+C++ is a vast language with many advanced features and libraries. 
+As you progress, you'll delve into more complex topics like templates, operator overloading,
+ memory management, and more. Always refer to up-to-date resources and 
+ documentation as you continue your C++ journey.
+
+*/
