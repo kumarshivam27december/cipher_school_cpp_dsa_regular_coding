8038418e8f500a3146c3eb73cb59a39c639340ec
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-08-11 18:53:54 +0530

    version 15.0

---
+++ day008/dsa_lec_1.cpp
@@ -0,0 +1,32 @@
+/*
+
+
+DSA 
+
+
+O(N^N)
+O(N!)
+O(N^k)
+O(N^2)
+O(N)
+O(1)
+
+
+code -> Talks to machine
+stories -> Talks to human 
+maths -> Talks to universe
+
+
+
+what is data structure ?
+
+select a data structure specific to a problem that we want to solve and specific to procedure 
+we are using to solve the problem.
+
+
+organise the data 
+storage that is used to store and organise data 
+way of organising the data.
+
+
+*/
